a120579370718a23a70d4911b97f79af69f54527
failure64 <swastikmiddha@gmail.com>
2023-07-26 16:42:28 +0530

    filters and categories added

--- ecom-admin/app/(root)/layout.tsx
+++ ecom-admin/app/(root)/layout.tsx
@@ -2,6 +2,7 @@ import { redirect } from 'next/navigation';
 import { auth } from '@clerk/nextjs';
 
 import prismadb from '@/lib/prismadb';
+import Navbar from '@/components/navbar';
 
 export default async function SetupLayout({
   children,
